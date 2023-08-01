a407169ec7c0fe72c9c70c76aefb631ea6e99fb6
Saurabh Gupta <saurabhji@me.com>
2023-08-01 14:39:07 +0530

    Labele - added custom test cases

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -61,8 +61,8 @@ Write your custom selenium scripts in `app/extension/jira/extension_ui.py`.
 Refer to `app/selenium_ui/jira/modules.py` for examples.
 """
 
-# def test_1_selenium_select_color_for_label(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+def test_1_selenium_select_color_for_label(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action(jira_webdriver, jira_datasets)
 
 
 # this action should be the last one
